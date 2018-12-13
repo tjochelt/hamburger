16bddffff172546d1e15431c35b87088cf9d864e
Tom Ocheltree <tom.ocheltree@gmail.com>
2018-12-12 22:21:03 -0600

    initial commit

---
+++ config/orm.js
@@ -0,0 +1,47 @@
+const connection = require("../config/connection.js");
+
+const orm = {
+  selectAll: function(whatToSelect, tableInput) {
+    var queryString = "SELECT ?? FROM ??";
+    connection.query(queryString, [whatToSelect, tableInput], function(
+      err,
+      result
+    ) {
+      if (err) throw err;
+      console.log(result);
+    });
+  },
+  insertOne: function(tableInput, colToSearch, valOfCol) {
+    var queryString = "SELECT * FROM ?? WHERE ?? = ?";
+
+    console.log(queryString);
+
+    connection.query(queryString, [tableInput, colToSearch, valOfCol], function(
+      err,
+      result
+    ) {
+      if (err) throw err;
+      console.log(result);
+    });
+  },
+  updateOne: function(
+    whatToSelect,
+    tableOne,
+    tableTwo,
+    onTableOneCol,
+    onTableTwoCol
+  ) {
+    var queryString = "SELECT ?? FROM ?? AS tOne";
+    queryString += " LEFT JOIN ?? AS tTwo";
+    queryString += " ON tOne.?? = tTwo.??";
+
+    console.log(queryString);
+
+    connection.query(queryString, function(err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+  }
+};
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers
+    (name)
+VALUES
+    ('Swushroom');
+INSERT INTO burgers
+    (name)
+VALUES
+    ('The Rex Montana');
+INSERT INTO burgers
+    (name)
+VALUES
+    ("Cheeseburger");
